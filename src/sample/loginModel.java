7587757da10269e369207e88d85159c126342aa5
PloyPiyanut <Inukson2538@gmail.com>
2018-02-12 16:51:33 +0700

    user login  error

--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -8,48 +8,45 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class loginModel {
+
     Connection connection;
 
     public loginModel() {
-
         try {
             this.connection = dbConnection.getConnection();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        if (this.connection == null) {
+        if (this.connection == null){
             System.exit(1);
         }
-
     }
-    public boolean isDatabaseConnection() {
+    public boolean isDatabaseConnection(){
         return this.connection != null;
     }
-
     public boolean isLogin(String user, String pass) throws SQLException {
         PreparedStatement pr = null;
         ResultSet rs = null;
 
-        String sql = "SELECT * FROM usesr where username = ? and" + "password = ?";
+        String sql = "SELECT * FROM user where username = ? and " + "Password = ?";
         try {
             pr = this.connection.prepareStatement(sql);
-            pr.setString(2, user);
-            pr.setString(3, user);
+            pr.setString(2,user);
+            pr.setString(3,pass);
 
             rs = pr.executeQuery();
 
-            if (rs.next()) {
+            if (rs.next()){
                 return true;
             }
-
-        } catch (SQLException ex) {
+            return false;
+        }catch (SQLException ex){
             ex.printStackTrace();
             return false;
         }finally {
             pr.close();
-            pr.close();
+            rs.close();
         }
-
 
 
 
