f66bf98f03f1abba78f6c79c09a3abb9b5232487
PloyPiyanut <Inukson2538@gmail.com>
2018-02-19 17:06:44 +0700

    Locd Data and Find

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -1,13 +1,12 @@
 package dbUtil;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConnection {
 
-    private static final String SQCONN = "jdbc:sqlite:\\school.sqlite";
+    private static final String SQCONN = "jdbc:sqlite:school.sqlite";
 
     public static Connection getConnection() throws SQLException {
         try {
--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -31,8 +31,8 @@ public class loginModel {
         String sql = "SELECT * FROM user where username = ? and " + "Password = ?";
         try {
             pr = this.connection.prepareStatement(sql);
-            pr.setString(2,user);
-            pr.setString(3,pass);
+            pr.setString(1,user);
+            pr.setString(2,pass);
 
             rs = pr.executeQuery();
 
